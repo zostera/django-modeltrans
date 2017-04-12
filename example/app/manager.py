8cd7d106d5da8dc7c75b3e772109ba68b3fb5292
Jan Pieter Waagmeester <jieter@jieter.nl>
2017-04-12 19:53:45 +0200

    Add more tests and a start on filtering

--- example/app/manager.py
+++ example/app/manager.py
@@ -1,40 +1,48 @@
+from django.core.exceptions import FieldError
 from django.db import models
 from django.db.models import CharField
 from django.db.models.expressions import RawSQL
-from django.db.models.functions import Coalesce
-
-
-def get_translatable_fields_for_model(model):
-    return getattr(model, 'translatable', None)
+from django.db.models.functions import Cast, Coalesce
 
 
 class MultilingualQuerySet(models.query.QuerySet):
-    def _post_init(self):
-        return
+    def get_translatable_fields(self):
+        return getattr(self.model, 'translatable', None)
 
-    def _rewrite_applied_operations(self):
-        return
+    def add_i18n_annotate(self, field_name, fallback=True):
+        '''
+        Add an annotation to the query to extract the translated verion of a field
+        from the jsonb field to allow filtering and ordering
+        '''
+        if '_' not in field_name:
+            return
+            # raise FieldError('Cannot extract a field ({}), it does not contain a language designator'.format(field_name))
+
+        # TODO: split on last _, not on first
+        original = field_name.split('_')[0]
+
+        if original in self.get_translatable_fields():
+            if fallback:
+                # fallback to the original untranslated field
+                field = Coalesce(RawSQL('i18n->>%s', (field_name, )), original, output_field=CharField())
+            else:
+                field = Cast(RawSQL('i18n->>%s', (field, )), CharField())
+
+            self.query.add_annotation(field, field_name)
 
     def order_by(self, *field_names):
         '''Annotate the queryset if a translated field is requested for sorting'''
-        translatable = get_translatable_fields_for_model(self.model)
 
         for field in field_names:
             # remove descending prefix to create the annotation
             if field[0] == '-':
                 field = field[1:]
-
-            if '_' in field:
-                base = field.split('_')[0]  # todo, split on last _, not on first
-
-                if base in translatable:
-                    # annotate query, with fallback on original field
-                    self.query.add_annotation(
-                        Coalesce(RawSQL('i18n->>%s', (field, )), base, output_field=CharField()),
-                        field
-                    )
+            self.add_i18n_annotate(field)
 
         return super(MultilingualQuerySet, self).order_by(*field_names)
+
+    def filter(self, *args, **kwargs):
+        return super(MultilingualQuerySet, self).filter(*args, **kwargs)
 
 
 def multilingual_queryset_factory(old_cls, instantiate=True):
@@ -55,6 +63,6 @@ class MultilingualQuerySetManager(models.Manager):
 
     def _patch_queryset(self, qs):
         qs.__class__ = multilingual_queryset_factory(qs.__class__, instantiate=False)
-        qs._post_init()
-        qs._rewrite_applied_operations()
+        # qs._post_init()
+        # qs._rewrite_applied_operations()
         return qs
--- example/app/tests.py
+++ example/app/tests.py
@@ -11,18 +11,53 @@ def key(queryset, key):
     return list([getattr(model, key) for model in queryset])
 
 
+class FilterTest(TestCase):
+    data = (
+        ('Falcon', 'Valk'),
+        ('Frog', 'Kikker'),
+        ('Toad', 'Pad'),
+        ('Duck', 'Eend'),
+        ('Dolphin', 'Dolfijn')
+    )
+
+    def setUp(self):
+        for title, title_nl in self.data:
+            BlogI18n.objects.create(title=title, i18n={'title_nl': title_nl})
+
+    def test_has_language(self):
+        '''Check if a certain field is translated in a certain language'''
+
+    def test_filter_contains(self):
+        '''
+        We want to do a text contains in translated value lookup
+        '''
+
+        qs = BlogI18n.objects.filter(title_nl__contains='al')
+
+        self.assertEquals(qs[0].title_nl, 'Valk')
+
+    def test_filter_contains_fallback(self):
+        pass
+
+
 class OrderByTest(TestCase):
     EN = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
     NL = ['A', 'B', 'C', 'D', 'Z', 'Y', 'X']
+    FR = ['1', '1', '1', '1', '2', '2', '2']
 
     def setUp(self):
         for i, en in enumerate(self.EN):
-            BlogI18n.objects.create(title=en, i18n={'title_nl': self.NL[i]})
+            BlogI18n.objects.create(title=en, i18n={'title_nl': self.NL[i], 'title_fr': self.FR[i]})
 
     def test_order_by_fails_for_normal_model(self):
-
         with self.assertRaises(FieldError):
             list(Blog.objects.all().order_by('title_nl'))
+
+    def test_order_by_two_fields(self):
+        '''Multiple translated fields should work too'''
+        qs = BlogI18n.objects.all().order_by('-title_fr', 'title_nl')
+
+        self.assertEquals(key(qs, 'title_nl'), 'X,Y,Z,A,B,C,D'.split(','))
 
     def test_order_asc(self):
         qs = BlogI18n.objects.all().order_by('title_nl')
@@ -33,3 +68,6 @@ class OrderByTest(TestCase):
     def test_order_desc(self):
         qs = BlogI18n.objects.all().order_by('-title_nl')
         self.assertEquals(key(qs, 'title_nl'), sorted(self.NL, reverse=True))
+
+        qs = BlogI18n.objects.all().order_by('-title')
+        self.assertEquals(key(qs, 'title'), sorted(self.EN, reverse=True))
