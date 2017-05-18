d859d0d6306c29faab0a08f8c05edec80f7447b3
bulat <bulatshfglh@gmail.com>
2017-05-18 17:46:58 +0300

    make local settings respect global settings

--- modeltrans/settings.py
+++ modeltrans/settings.py
@@ -2,7 +2,7 @@ from django.conf import settings
 
 DEBUG = getattr(settings, 'MODELTRANS_DEBUG', False)
 
-DEFAULT_LANGUAGE = 'en'
-ENABLE_REGISTRATIONS = True
+DEFAULT_LANGUAGE = settings.DEFAULT_LANGUAGE or 'en'
+ENABLE_REGISTRATIONS = settings.ENABLE_REGISTRATIONS or True
 
-AVAILABLE_LANGUAGES = ('nl', 'de', 'fr')
+AVAILABLE_LANGUAGES = settings.AVAILABLE_LANGUAGES or ('nl', 'de', 'fr')
