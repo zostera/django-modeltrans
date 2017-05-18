78f9ade03c059ca78b236582c2adaab932c8236c
bulat <bulatshfglh@gmail.com>
2017-05-18 18:36:15 +0300

    app settings overwrites global settings (#10)
    * make local settings respect global settings
    
    * make local settings respect global settings: fix case when value is not set by default

--- modeltrans/settings.py
+++ modeltrans/settings.py
@@ -2,7 +2,7 @@ from django.conf import settings
 
 DEBUG = getattr(settings, 'MODELTRANS_DEBUG', False)
 
-DEFAULT_LANGUAGE = 'en'
-ENABLE_REGISTRATIONS = True
+DEFAULT_LANGUAGE = getattr(settings, 'DEFAULT_LANGUAGE', 'en')
+ENABLE_REGISTRATIONS = getattr(settings, 'ENABLE_REGISTRATIONS', True)
 
-AVAILABLE_LANGUAGES = ('nl', 'de', 'fr')
+AVAILABLE_LANGUAGES = getattr(settings, 'AVAILABLE_LANGUAGES', ('nl', 'de', 'fr'))
