e65d3fc0f69d138274688783cb4a06d78469be7d
Jan Pieter Waagmeester <jieter@jieter.nl>
2017-06-23 17:32:14 +0200

    Add more debug info about the env.

--- test_migrations/manage.py
+++ test_migrations/manage.py
@@ -4,6 +4,9 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "migrate_test.settings")
+    for key, val in os.environ.items():
+        print(key, val)
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
