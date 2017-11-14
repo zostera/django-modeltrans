77ef6ab84d5396021c2c1d8c57cd5a16c010b9c3
Jan Pieter Waagmeester <jieter@jieter.nl>
2017-11-14 12:24:39 +0100

    Allow login without authentication in admin and add limited admin mixin

--- example/app/admin.py
+++ example/app/admin.py
@@ -3,7 +3,12 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from modeltrans.admin import ActiveLanguageMixin
+
 from .models import Blog, Category
+from .utils import disable_admin_login
+
+admin.site.has_permission = disable_admin_login()
 
 
 @admin.register(Blog)
@@ -11,6 +16,21 @@ class BlogAdmin(admin.ModelAdmin):
     pass
 
 
+
+class DefaultAdminCategory(Category):
+    '''
+    Proxy model to have both the unlimited version of the ModelAdmin and
+    a limited version
+    '''
+    class Meta:
+        proxy = True
+
+
+@admin.register(DefaultAdminCategory)
+class DefaultCategoryAdmin(admin.ModelAdmin):
+    pass
+
+
 @admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
+class CategoryAdmin(ActiveLanguageMixin, admin.ModelAdmin):
     pass
