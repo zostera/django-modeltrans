400fc920d38717b3b84a611624244003f3db50a7
Jan Pieter Waagmeester <jieter@zostera.nl>
2018-07-06 10:13:51 +0200

    Also verify the category name is in the response

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -34,14 +34,17 @@ class AdminTest(TestCase):
         for url in urls:
             response = self.client.get(url)
             self.assertEqual(response.status_code, 200)
+            self.assertContains(response, self.wikipedia.name)
 
             with override("nl"):
                 response = self.client.get(url)
                 self.assertEqual(response.status_code, 200)
+                self.assertContains(response, self.wikipedia.name)
 
             with override("de"):
                 response = self.client.get(url)
                 self.assertEqual(response.status_code, 200)
+                self.assertContains(response, self.wikipedia.name)
 
     def test_non_translated_admin(self):
         url = reverse("admin:app_site_change", args=(self.site.pk,))
