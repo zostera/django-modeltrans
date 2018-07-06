4613daea5d9d603b5f092005627fabd805de8a45
Jan Pieter Waagmeester <jieter@zostera.nl>
2018-07-06 09:09:04 +0200

    Make initial ./manage.py migrate work in example

--- example/app/utils.py
+++ example/app/utils.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.db.utils import ProgrammingError
 
 
 def disable_admin_login():
@@ -9,16 +10,20 @@ def disable_admin_login():
     """
     User = get_user_model()
 
-    user, created = User.objects.update_or_create(
-        id=1,
-        defaults=dict(
-            first_name="Default Admin",
-            last_name="User",
-            is_superuser=True,
-            is_active=True,
-            is_staff=True,
-        ),
-    )
+    try:
+        user, created = User.objects.update_or_create(
+            id=1,
+            defaults=dict(
+                first_name="Default Admin",
+                last_name="User",
+                is_superuser=True,
+                is_active=True,
+                is_staff=True,
+            ),
+        )
+    except ProgrammingError:
+        # auth_user doesn't exist, this allows the migrations to run properly.
+        user = None
 
     def no_login_has_permission(request):
         setattr(request, "user", user)
