4192c9bca862ca04d0b98b9458f53998c061158c
Jan Pieter Waagmeester <jieter@zostera.nl>
2018-07-16 09:02:46 +0200

    Add tests for utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,15 +2,24 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from django.utils.translation import override
+from tests.app.models import Blog
 
 from modeltrans.manager import transform_translatable_fields
-from modeltrans.utils import build_localized_fieldname, split_translated_fieldname
-from tests.app.models import Blog
+from modeltrans.utils import build_localized_fieldname, get_language, split_translated_fieldname
 
 
 class UtilsTest(TestCase):
-    def test_split_translated_fieldname(self):
+    def test_get_language(self):
+        self.assertEqual(get_language(), "en")
 
+        with override("nl"):
+            self.assertEqual(get_language(), "nl")
+
+        with override("id"):
+            self.assertEqual(get_language(), "en")
+
+    def test_split_translated_fieldname(self):
         self.assertEqual(split_translated_fieldname("title_nl"), ("title", "nl"))
 
         self.assertEqual(split_translated_fieldname("full_name_nl"), ("full_name", "nl"))
@@ -24,3 +33,5 @@ class UtilsTest(TestCase):
     def test_build_localized_fieldname(self):
         self.assertEqual(build_localized_fieldname("title", "nl"), "title_nl")
         self.assertEqual(build_localized_fieldname("category__name", "nl"), "category__name_nl")
+        self.assertEqual(build_localized_fieldname("title", "id"), "title_ind")
+        self.assertEqual(build_localized_fieldname("title", "en-US"), "title_en_US")
