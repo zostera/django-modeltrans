zostera/django-modeltrans

commit 3cb846d2bc0f1884653b44a3a768cfd1c67cd801

Use `TranslationModelForm` to implement `ActiveLanguageMixin` (#80)

modeltrans/admin.py
@@ -1,35 +1,9 @@
-from .conf import get_default_language
-from .translator import get_i18n_field
-from .utils import get_language
+from .forms import TranslationModelForm
 
 
-class ActiveLanguageMixin(object):
+class ActiveLanguageMixin:
     """
-    Add this mixin to your admin class to exclude all virtual fields, except:
-
-     - The original field (for the default language, settings.LANGUAGE_CODE)
-     - The field for the currently active language, without fallback.
+    ModelAdmin mixin to only show fields for the fallback and current language in the change view.
     """
 
-    def get_exclude(self, request, obj=None):
-        i18n_field = get_i18n_field(self.model)
-        # use default implementation for models without i18n-field
-        if i18n_field is None:
-            return super().get_exclude(request)
-
-        language = get_language()
-        if language == get_default_language():
-            language = False
-
-        excludes = []
-        for field in i18n_field.get_translated_fields():
-            # Not excluded:
-            # - language is None: the _i18n-version of the field.
-            # - language equals the current language
-            if field.language == language:
-                continue
-
-            excludes.append(field.name)
-
-        # de-duplicate
-        return list(set(excludes))
+    form_class = TranslationModelForm

commit 14bada6999435d868e7911daec0996a8c08dc9ef

Fix regression in ActiveLanguageMixin introduced in #80

.gitignore
@@ -15,3 +15,4 @@ __pycache__
 dist/
 
 .idea
+.vscode

modeltrans/admin.py
@@ -6,4 +6,4 @@ class ActiveLanguageMixin:
     ModelAdmin mixin to only show fields for the fallback and current language in the change view.
     """
 
-    form_class = TranslationModelForm
+    form = TranslationModelForm

modeltrans/forms.py
@@ -1,6 +1,7 @@
 import itertools
 
 from django import forms
+from django.forms.models import ALL_FIELDS
 from django.utils.translation import gettext_lazy as _
 
 from .conf import get_available_languages, get_default_language
@@ -20,19 +21,19 @@ class TranslationModelFormOptions(forms.models.ModelFormOptions):
         self.fallback_language = getattr(options, "fallback_language", None)
 
 
-class TranslationModelFormMetaClass(forms.models.ModelFormMetaclass):
+class TranslationModelFormMetaclass(forms.models.ModelFormMetaclass):
     def __new__(mcs, name, bases, attrs):
         """
         Include all translation fields for translatable fields declared in the form.
 
-        We use the standard ModelForm field declaration procedure of the ModelFormMetaClass,
+        We use the standard ModelForm field declaration procedure of the ModelFormMetaclass,
         so that all fields are declared during creation of the new object.
 
         Different actions are required for different Meta class options:
-        1) fields - if fields are declared explicitly with the fields option, the related i18n fields will not be
-        included, and these have to be added to base_fields.
-        2) exclude - if fields are declared implicitly with the exclude option, the i18n translation fields, as well as
-        the i18n field itself, will be included. We will remove the i18n field.
+        1) fields - if fields are declared explicitly with the fields option,
+           the related i18n fields will not be included, and these have to be added to base_fields.
+        2) exclude - if fields are declared implicitly with the exclude option,
+           the i18n translation fields, as well as the i18n field itself, will be included. We will remove the i18n field.
 
         Here all languages are included, so that in the form we can curate which fields are available according to
         settings of the form instance (e.g. kwarg overrides, or model instance fallbacks).
@@ -49,95 +50,83 @@ class TranslationModelFormMetaClass(forms.models.ModelFormMetaclass):
 
         # get options, with extra translation form options
         opts = new_class._meta = TranslationModelFormOptions(getattr(new_class, "Meta", None))
-        model_class = opts.model
 
-        languages = get_available_languages()
+        if not opts.model:
+            return new_class
+
+        i18n_field = get_i18n_field(opts.model)
+        if not i18n_field:
+            return new_class
 
         # Note that base_fields generated by ModelForm do not yet include translation fields if generated with "fields"
         # Meta option, but includes all translation fields if generated with "excludes" Meta option.
         base_fields = list(new_class.base_fields.keys())
-
         # convert to list in case it is a tuple
-        opts_fields = list(opts.fields) if opts.fields else None
+        opts_fields = list(opts.fields) if opts.fields and opts.fields != ALL_FIELDS else None
         opts_exclude = list(opts.exclude) if opts.exclude else None
 
-        if model_class:
-            i18n_field = get_i18n_field(model_class)
-            if i18n_field:
+        languages = get_available_languages()
 
-                for original_field_name in i18n_field.fields:  # for all translated fields
+        for original_name in i18n_field.fields:  # for all translated fields
 
-                    # for all possible system languages
-                    for language in languages:
-                        field_name = build_localized_fieldname(
-                            original_field_name, language, ignore_default=True
-                        )
+            # for all possible system languages
+            for language in languages:
+                field_name = build_localized_fieldname(original_name, language, ignore_default=True)
 
-                        # add i18n field if an explicitly chosen field
-                        if (
-                            opts.fields
-                            and original_field_name in base_fields
-                            and field_name not in base_fields
-                        ):
-                            base_fields.append(field_name)
-                            opts_fields.append(field_name)
+                # add i18n field if an explicitly chosen field
+                if opts.fields and original_name in base_fields and field_name not in base_fields:
+                    base_fields.append(field_name)
+                    opts_fields.append(field_name)
 
-                        # remove field if an explicitly excluded field
-                        if (
-                            opts.exclude
-                            and original_field_name in opts.exclude
-                            and field_name in base_fields
-                        ):
-                            base_fields.remove(field_name)
-                            opts_exclude.append(field_name)
+                # remove field if an explicitly excluded field
+                if opts.exclude and original_name in opts.exclude and field_name in base_fields:
+                    base_fields.remove(field_name)
+                    opts_exclude.append(field_name)
 
-                    # Remove the i18n field if present (e.g. because of using the exclude option)
-                    name = f"{original_field_name}_i18n"
-                    if name in base_fields:
-                        base_fields.remove(name)
-                    if opts.exclude:
-                        opts_exclude.append(name)
+            # Remove the i18n field if present (e.g. because of using the exclude option)
+            name = f"{original_name}_i18n"
+            if name in base_fields:
+                base_fields.remove(name)
+            if opts.exclude:
+                opts_exclude.append(name)
 
-                    # Remove the i18n field for the system default language, because that already exists as the default
-                    name = f"{original_field_name}_{get_default_language()}"
-                    if name in base_fields:
-                        base_fields.remove(name)
-                    if opts.fields and name in opts.fields:
-                        opts_fields.remove(name)
-                    if opts.exclude and name not in opts.exclude:
-                        opts_exclude.append(name)
+            # Remove the translation field for the system default language, because that already exists as the original field
+            name = f"{original_name}_{get_default_language()}"
+            if name in base_fields:
+                base_fields.remove(name)
+            if opts.fields and name in opts.fields:
+                opts_fields.remove(name)
+            if opts.exclude and name not in opts.exclude:
+                opts_exclude.append(name)
 
-                opts.fields = opts_fields
-                opts.exclude = opts_exclude
+        opts.fields = opts_fields
+        opts.exclude = opts_exclude
 
-                base_fields = forms.fields_for_model(
-                    opts.model,
-                    opts.fields,
-                    opts.exclude,
-                    opts.widgets,
-                    formfield_callback,
-                    opts.localized_fields,
-                    opts.labels,
-                    opts.help_texts,
-                    opts.error_messages,
-                    opts.field_classes,
-                    # limit_choices_to will be applied during ModelForm.__init__().
-                    apply_limit_choices_to=False,
-                )
+        base_fields = forms.fields_for_model(
+            opts.model,
+            opts.fields,
+            opts.exclude,
+            opts.widgets,
+            formfield_callback,
+            opts.localized_fields,
+            opts.labels,
+            opts.help_texts,
+            opts.error_messages,
+            opts.field_classes,
+            # limit_choices_to will be applied during ModelForm.__init__().
+            apply_limit_choices_to=False,
+        )
+        # Override default model fields with any custom declared ones
+        # (plus, include all the other declared fields).
+        base_fields.update(new_class.declared_fields)
 
-                # Override default model fields with any custom declared ones
-                # (plus, include all the other declared fields).
-                base_fields.update(new_class.declared_fields)
-            else:
-                base_fields = new_class.declared_fields
-
-            # Override base_fields with properly determined set of all translation fields based on ModelForm
-            new_class.base_fields = base_fields
+        # Override base_fields with properly determined set of all translation fields based on ModelForm
+        new_class.base_fields = base_fields
 
         return new_class
 
 
-class TranslationModelForm(forms.ModelForm, metaclass=TranslationModelFormMetaClass):
+class TranslationModelForm(forms.ModelForm, metaclass=TranslationModelFormMetaclass):
     """
     Model form that only adds translation fields for specified languages.
 
@@ -202,9 +191,12 @@ class TranslationModelForm(forms.ModelForm, metaclass=TranslationModelFormMetaCl
 
         self.model_i18n_field = get_i18n_field(self._meta.model)
         self.languages = languages or self._meta.languages
-        self.i18n_fields = [
-            field for field in self.model_i18n_field.fields if field in self.base_fields.keys()
-        ]
+        if self.model_i18n_field:
+            self.i18n_fields = [
+                field for field in self.model_i18n_field.fields if field in self.base_fields.keys()
+            ]
+        else:
+            self.i18n_fields = []
 
         # Given that we set opt.fields and opts.exclude in Meta,
         # the setting of initial values that occurs in ModelForm includes
@@ -232,7 +224,7 @@ class TranslationModelForm(forms.ModelForm, metaclass=TranslationModelFormMetaCl
                 build_localized_fieldname(original_field, language_code, ignore_default=True)
                 for language_code in self.language_codes
             ]
-        fields["__all__"] = list(itertools.chain.from_iterable(fields.values()))
+        fields[ALL_FIELDS] = list(itertools.chain.from_iterable(fields.values()))
         return fields
 
     def set_translation_field_attributes(self):
@@ -277,7 +269,7 @@ class TranslationModelForm(forms.ModelForm, metaclass=TranslationModelFormMetaCl
             field_order = new_field_order
         else:
             # if no explicit field order adopt the order of i18n form fields used to generate the included fields
-            field_order = self.included_fields["__all__"]
+            field_order = self.included_fields[ALL_FIELDS]
 
         self.order_fields(field_order)
 
@@ -286,7 +278,7 @@ class TranslationModelForm(forms.ModelForm, metaclass=TranslationModelFormMetaCl
         fields = self.fields.copy()
         for original_field_name in self.i18n_fields:
             for field in fields:
-                if original_field_name in field and field not in self.included_fields["__all__"]:
+                if original_field_name in field and field not in self.included_fields[ALL_FIELDS]:
                     self.fields.pop(field)
 
     def get_fallback_language(self, fallback_language=None):
@@ -305,7 +297,11 @@ class TranslationModelForm(forms.ModelForm, metaclass=TranslationModelFormMetaCl
         if self._meta.fallback_language:
             return self._meta.fallback_language
 
-        if self.model_i18n_field.fallback_language_field and self.instance.pk:
+        if (
+            self.model_i18n_field
+            and self.model_i18n_field.fallback_language_field
+            and self.instance.pk
+        ):
             return get_instance_field_value(
                 self.instance, self.model_i18n_field.fallback_language_field
             )

setup.py
@@ -34,7 +34,7 @@ setup(
     url="https://github.com/zostera/django-modeltrans/",
     packages=find_packages(exclude=["tests.*", "tests", "example.*", "example"]),
     include_package_data=True,  # declarations in MANIFEST.in
-    install_requires=["Django>=1.11.15"],
+    install_requires=["Django>=2.2"],
     classifiers=[
         "Environment :: Web Environment",
         "Framework :: Django",

tests/test_admin.py
@@ -27,24 +27,34 @@ class AdminTest(TestCase):
         self.wikipedia = Category.objects.get(name="Wikipedia")
 
     def test_limited_admin(self):
-        urls = [
-            reverse("admin:app_category_changelist"),
-            reverse("admin:app_category_change", args=(self.wikipedia.pk,)),
-        ]
-        for url in urls:
+        url = reverse("admin:app_category_changelist")
+
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.wikipedia.name)
+
+        with override("nl"):
             response = self.client.get(url)
             self.assertEqual(response.status_code, 200)
             self.assertContains(response, self.wikipedia.name)
 
-            with override("nl"):
-                response = self.client.get(url)
-                self.assertEqual(response.status_code, 200)
-                self.assertContains(response, self.wikipedia.name)
+        with override("de"):
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, 200)
+            self.assertContains(response, self.wikipedia.name)
 
-            with override("de"):
-                response = self.client.get(url)
-                self.assertEqual(response.status_code, 200)
-                self.assertContains(response, self.wikipedia.name)
+    def test_change_view(self):
+        url = reverse("admin:app_category_change", args=(self.wikipedia.pk,))
+
+        with override("nl"):
+            response = self.client.get(url)
+            self.assertNotContains(response, "name_de")
+            self.assertContains(response, "name_nl")
+
+        with override("de"):
+            response = self.client.get(url)
+            self.assertNotContains(response, "name_nl")
+            self.assertContains(response, "name_de")
 
     def test_non_translated_admin(self):
         url = reverse("admin:app_site_change", args=(self.site.pk,))

tests/test_forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from django.forms import ModelForm
+from django.forms import ModelForm, modelform_factory
+from django.forms.models import ALL_FIELDS
 from django.test import TestCase
 from django.utils.translation import override
 
@@ -72,9 +73,8 @@ class TranslationModelFormTestCase(TestCase):
         """Test the error messages for incorrect languages options."""
 
         for language in [0, "es_it", "e", "xx"]:
-            with self.assertRaisesMessage(
-                ValueError, f"languages: value {language} is not permitted"
-            ):
+            expected_message = f"languages: value {language} is not permitted"
+            with self.assertRaisesMessage(ValueError, expected_message):
                 Form(languages=[language])
 
         class NoLanguageForm(TranslationModelForm):
@@ -93,6 +93,17 @@ class TranslationModelFormTestCase(TestCase):
         form = Form()
         self.assertEqual(form.languages, ["browser", "fallback"])
         self.assertEqual(form.fallback_language, get_default_language())
+
+    def test_all_fields(self):
+        class Form(TranslationModelForm):
+            class Meta:
+                model = Challenge
+                fields = ALL_FIELDS
+
+        Form().as_p()
+
+        Form = modelform_factory(model=Challenge, form=TranslationModelForm, fields=ALL_FIELDS)
+        Form().as_p()
 
     def test_get_fallback_language(self):
         """Test that the form fallback language is set correctly."""