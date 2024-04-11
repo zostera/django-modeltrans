9e713cd22f3ebe316c943d566f759dc317b2efb7
Jan Pieter Waagmeester <jieter@waarneming.nl>
2024-04-11 21:26:55 +0200

    Simpler

--- test_migrations/migrate_test/settings.py
+++ test_migrations/migrate_test/settings.py
@@ -72,7 +72,7 @@ WSGI_APPLICATION = "migrate_test.wsgi.application"
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": "modeltrans-migration",
         "USER": "postgres",
         "PASSWORD": "test1234",
