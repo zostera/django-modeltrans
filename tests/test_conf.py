9feacf1bdc5aa03b7167be102c034bb36a1e1d08
Jacob Wegner <jacobwegner@gmail.com>
2024-05-10 12:05:34 -0500

    Add test demonstrating decoupling from LANGUAGE_CODE

--- tests/test_conf.py
+++ tests/test_conf.py
@@ -2,6 +2,9 @@ from django.core.exceptions import ImproperlyConfigured
 from django.test import TestCase, override_settings
 
 from modeltrans.conf import check_fallback_chain, get_available_languages_setting
+from modeltrans.translator import check_languages, get_i18n_field, get_i18n_field_param
+
+from .app.models import Person
 
 
 class FallbackConfTest(TestCase):
@@ -37,3 +40,29 @@ class FallbackConfTest(TestCase):
         message = "MODELTRANS_AVAILABLE_LANGUAGES should be an iterable of strings"
         with self.assertRaisesMessage(ImproperlyConfigured, message):
             get_available_languages_setting()
+
+
+class DefaultLanguageConfTest(TestCase):
+    @override_settings(
+        LANGUAGE_CODE="es", MODELTRANS_AVAILABLE_LANGUAGES=("nl", "de", "fr")
+    )
+    def test_django_language_code_not_available_language(self):
+        message = 'Language "en" is in required_languages on Model "Person" but not in settings.MODELTRANS_AVAILABLE_LANGUAGES.'
+        i18n_field = get_i18n_field(Person)
+        required_languages = get_i18n_field_param(
+            Person, i18n_field, "required_languages"
+        )
+        with self.assertRaisesMessage(ImproperlyConfigured, message):
+            check_languages(required_languages, Person)
+
+    @override_settings(
+        LANGUAGE_CODE="es",
+        MODELTRANS_DEFAULT_LANGUAGE="en",
+        MODELTRANS_AVAILABLE_LANGUAGES=("nl", "de", "fr"),
+    )
+    def test_default_language_code_is_available_language(self):
+        i18n_field = get_i18n_field(Person)
+        required_languages = get_i18n_field_param(
+            Person, i18n_field, "required_languages"
+        )
+        check_languages(required_languages, Person)
